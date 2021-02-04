c773645db8c36ad5fdbc5d39eedbf574bfbb9374
Madjid Taoualit <madjid.taoualit@etu.univ-lehavre.fr>
2021-02-04 14:25:00 +0100

    Updating (index.js) -> Creating export constant function Enumeration

--- src/index.js
+++ src/index.js
@@ -1,3 +1,24 @@
+/*
+ *@Author Madjid Taoualit
+ * M1-IWOCS-2021
+ */
+/* Coded using  ECMASript 6 (class) Pattern */
+
+
 export const version = () => '1.0.0';
 
-/* TODO : Créer le modèle objet ici */
+/* Creating export constant function Enumeration */
+
+export const Enumeration = function (keys) {
+  const enumeration = Object.create(null);
+  for (const key of keys) {
+    enumeration[key] = key;
+  }
+  enumeration[Symbol.iterator] = function* () {
+    for (const key of keys) {
+      yield enumeration[key];
+    }
+  };
+  Object.freeze(enumeration);
+  return enumeration;
+};
